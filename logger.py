c4f7165c455b2c56a141dcbb7d86d4a9d48110ea
senjoeson <senjoeson@foxmai.com>
2020-11-28 20:50:36 +0800

    修复启动时,未自动创建log文件夹

--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import logging
 import os.path
 import time
-from constant import LOG_FILE_SWITCH,LOG_CONSOLE_LEVEL,LOG_FILE_LEVEL
+from constant import LOG_FILE_SWITCH, LOG_CONSOLE_LEVEL, LOG_FILE_LEVEL
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
@@ -13,6 +13,8 @@ logger.setLevel(level=logging.DEBUG)
 time_line = time.strftime('%Y%m%d_%H%M', time.localtime(time.time()))
 
 log_path = os.getcwd() + '\log\\'
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
 logfile = log_path + time_line + '.log'
 handler = logging.FileHandler(logfile, mode='w')
 handler.setLevel(LOG_FILE_LEVEL)
@@ -25,7 +27,6 @@ console.setLevel(LOG_CONSOLE_LEVEL)
 if LOG_FILE_SWITCH:
     logger.addHandler(handler)
 logger.addHandler(console)
-
 
 if __name__ == '__main__':
     logger.debug('This is a debug message.')
